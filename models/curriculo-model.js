90c5756a4bcf9601be609949bf22a29801e2e151
rheimon1 <wallacerheimon@gmail.com>
2019-10-10 15:54:03 -0300

    2nd commit

---
+++ models/curriculo-model.js
@@ -0,0 +1,23 @@
+const data = {
+        title: 'Meu currículo',
+        name: 'Wallace Ramon',
+        profession: 'Software Engineer',
+        description: 'Experiência em desenvolvimento de APIs em Javascript.',
+        experience: [{
+            company: 'Opportunity Soluções Digitais',
+            office: 'Estagiário',
+            description: 'xxxx'
+        },
+        {
+            company: 'GOU',
+            office: 'Estagiário',
+            description: 'xxxx'
+        }],
+        education: [{
+            institution: 'Instituto Federal Norte de Minas Gerais - Câmpus Salinas',
+            description: 'Técnico em Informática'
+        }],
+        skills: ['backend', 'frontend']
+};
+
+module.exports = data;
